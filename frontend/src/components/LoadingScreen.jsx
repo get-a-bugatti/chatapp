2eb7aaa6d2adea6ede1650fec5c46268575e2d78
get-a-bugatti <37701415+get-a-bugatti@users.noreply.github.com>
2026-06-26 21:39:34 +0545

    Added Loading... text in LoadingScreen component

--- frontend/src/components/LoadingScreen.jsx
+++ frontend/src/components/LoadingScreen.jsx
@@ -4,6 +4,7 @@ export default function LoadingScreen() {
   return (
     <div className="flex h-screen items-center justify-center bg-white dark:bg-gray-950">
       <Spinner />
+      <p className="text-gray-500 font-semibold">Loading...</p>
     </div>
   );
 
