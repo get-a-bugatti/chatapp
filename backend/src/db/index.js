5c8107687f093ebe5b85dbd3546047b66d9cd666
get-a-bugatti <37701415+get-a-bugatti@users.noreply.github.com>
2026-06-29 12:58:10 +0545

    minor changes

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -7,7 +7,6 @@ export const connectDb = async () => {
       `mongodb+srv://k3nny_db_user:%40Manis20620926@cluster0.k702xdq.mongodb.net/${DB_NAME}?appName=Cluster0`
     );
   } catch (error) {
-    console.error("MongoDB Error :", error);
     throw new Error(error.message);
   }
 };
