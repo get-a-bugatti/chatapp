398781ee75af482ab7e11c0c0928ac9f7122d79f
get-a-bugatti <37701415+get-a-bugatti@users.noreply.github.com>
2026-06-30 11:39:19 +0545

    added nodemailer utility

---
+++ backend/src/utils/nodemailer.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendEmail = async (email, resetToken) => {
+  const mailOptions = {
+    from: `"Your Chat App Security" <>`,
+    to: targetEmail,
+    subject: "Your Password Reset OTP",
+    text: `Your password reset code is ${resetToken}. It will expire in 10 minutes.`,
+    html: `
+          <div style="font-family: sans-serif; padding: 20px; text-align: center;">
+            <h2>Password Reset Request</h2>
+            <p>Use the code below to complete your reset process:</p>
+            <h1 style="letter-spacing: 5px; color: #4A90E2;">${resetToken}</h1>
+            <p>This code expires in 10 minutes. If you did not request this, ignore this email.</p>
+          </div>
+        `,
+  };
+
+  // Send the email
+  await transporter.sendMail(mailOptions);
+};
