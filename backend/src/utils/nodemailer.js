df3f12581e6ed174d227d8cd1fc6a6e8ef73d618
get-a-bugatti <37701415+get-a-bugatti@users.noreply.github.com>
2026-07-02 20:45:26 +0545

    small changes to service and host for ndoemailer

--- backend/src/utils/nodemailer.js
+++ backend/src/utils/nodemailer.js
@@ -1,8 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
+  service: "gmail",
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
